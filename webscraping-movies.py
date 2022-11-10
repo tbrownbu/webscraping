78c4e676ebb244fb3d7c1b343a1d00f5a4379d98
Todd <Todd_brown1@baylor.edu>
2022-11-10 13:18:29 -0600

    movie

--- webscraping-movies.py
+++ webscraping-movies.py
@@ -5,9 +5,6 @@ import openpyxl as xl
 from openpyxl.styles import Font
 
 
-
-
-
 #webpage = 'https://www.boxofficemojo.com/weekend/chart/'
 webpage = 'https://www.boxofficemojo.com/year/2022/'
 
@@ -20,19 +17,23 @@ title = soup.title
 print(title.text)
 
 table_row = soup.findAll('tr')
-
-for row in table_row[0:4]:
+ranking = 1
+for row in table_row[1:6]:
     td=row.findAll("td")
-    rank=row[0].text
-    release = row[1].text
-    gross = row[5].text
-    theaters = row[6].text
-    total_gross = row[7].text
-    distributor = row[8].text
-    print('Rank: ',rank)
-    
-
-
+    rank=td[0].text
+    if int(rank)<8:
+        release = td[1].text
+        gross = td[5].text
+        theaters = td[6].text
+        total_gross = td[7].text
+        distributor = td[8].text
+        print('Rank: ',rank)
+        print('release: ',release)
+        print('gross: ',gross)
+        print('theaters: ',theaters)
+        print('total gross: ',total_gross)
+        print('distributor: ',distributor)
+    ranking+=1
 
 ##
 ##
